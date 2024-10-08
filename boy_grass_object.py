41cc24ca463d81b2ed4147b710875eabada3f7a1
dongPC@notebook <96215716+donggeon15@users.noreply.github.com>
2024-10-08 12:05:28 +0900

    자료 추가

---
+++ boy_grass_object.py
@@ -0,0 +1,27 @@
+from pico2d import *
+
+# Game object class here
+class Grass:
+    pass
+
+class Boy:
+    pass
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+open_canvas()
+
+# initialization code
+
+# game main loop code
+
+# finalization code
+
+close_canvas()
